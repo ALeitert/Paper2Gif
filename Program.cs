55096a4582942f1f9713d243892f079eac1c3c41
Arne (T440s) <aleitert@kent.edu>
2021-07-31 05:08:40 +0200

    Added helper function to run terminal commands.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Diagnostics;
+using System.IO;
+
 
 namespace Paper2Gif
 {
@@ -7,6 +10,29 @@ namespace Paper2Gif
         static void Main(string[] args)
         {
             Console.WriteLine("*** Paper 2 Code ***");
+        }
+
+        // Helper function to run terminal commands.
+        // Returns the output given by the program.
+        string RunTerminalCommand(string prog, string args, string dir)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo()
+            {
+                FileName = prog,
+                WorkingDirectory = dir,
+                Arguments = args,
+                RedirectStandardOutput = true,
+                RedirectStandardError = false,
+                UseShellExecute = false
+            };
+
+            Process termProg = Process.Start(psi);
+
+            StreamReader outReader = termProg.StandardOutput;
+            string output = outReader.ReadToEnd();
+            outReader.Dispose();
+
+            return output;
         }
     }
 }
