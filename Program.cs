7ef27a86be11003a8d6963b931b8d4fe5add4bdb
Arne (T440s) <aleitert@kent.edu>
2021-07-31 04:54:25 +0200

    Added 'empty' program file.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Paper2Gif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Paper 2 Code ***");
+        }
+    }
+}
